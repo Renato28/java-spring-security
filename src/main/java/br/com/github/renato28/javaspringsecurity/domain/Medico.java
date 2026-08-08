1233da9e9548d50118ae58a7ad721e839042f9bb
Renato Nobrega <renatonobrega87@gmail.com>
2026-08-08 18:34:32 -0300

    Implementando a entidade Medico

---
+++ src/main/java/br/com/github/renato28/javaspringsecurity/domain/Medico.java
@@ -0,0 +1,81 @@
+package br.com.github.renato28.javaspringsecurity.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "medicos")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Deprecated
+    public Medico() {}
+
+    public Medico(DadosCadastroMedico dados) {
+        atualizadosDados(dados);
+    }
+
+    public void atualizadosDados(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+}
