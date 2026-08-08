d82d7e00e32bc5fd1068bb92cc4e1b0968ce0695
Renato Nobrega <renatonobrega87@gmail.com>
2026-08-08 18:19:51 -0300

    criando projeto e suas dependencias

---
+++ src/test/java/br/com/github/renato28/javaspringsecurity/JavaSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.renato28.javaspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
