7a3b120dcaf7463da0b64265972c838cb2f36b29
Renato Nobrega <renatonobrega87@gmail.com>
2026-08-08 19:01:28 -0300

    Implementando a excecao de regras de negocio

---
+++ src/main/java/br/com/github/renato28/javaspringsecurity/domain/RegraDeNegocioException.java
@@ -0,0 +1,7 @@
+package br.com.github.renato28.javaspringsecurity.domain;
+
+public class RegraDeNegocioException extends RuntimeException {
+    public RegraDeNegocioException(String message) {
+        super(message);
+    }
+}
