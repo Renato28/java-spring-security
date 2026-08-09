80c17f3f44fa366f1d17fe94f51231e27f0c6c79
Renato Nobrega <renatonobrega87@gmail.com>
2026-08-09 17:13:25 -0300

    implementando controller de index

---
+++ src/main/java/br/com/github/renato28/javaspringsecurity/controller/IndexController.java
@@ -0,0 +1,15 @@
+package br.com.github.renato28.javaspringsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/", "/home", "/index"})
+public class IndexController {
+
+    @GetMapping
+    public String carregarPaginaIndex() {
+        return "index";
+    }
+}
